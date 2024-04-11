ad4add4a2449f823258e43cce7d7b86c8b89c791
waasimAnsari786 <waasimansari92@gmail.com>
2024-04-11 04:46:31 -0700

    udate style.css

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
+// my all functions
+const printInch = () => {
+    console.log(`inch`);
+};
+
+const printFeet = () => {
+    console.log(`feet`);
+};
+const printMeter = () => {
+    console.log(`meter`);
+};
+const printKilometer = () => {
+    console.log(`kilometer`);
+};
+const printCentimeter = () => {
+    console.log(`centimeter`);
+};
+
+const targetElemet = (elemet) => {
+    if (elemet.value === `Inch`) {
+        printInch();
+    }
+
+    else if (elemet.value === `Feet`) {
+        printFeet();
+    }
+
+    else if (elemet.value === `Meter`) {
+        printMeter();
+    }
+
+    else if (elemet.value === `Kilometer`) {
+        printKilometer();
+    }
+
+    else if (elemet.value === `Centimeter`) {
+        printCentimeter();
+    }
+};
+
 //  my all variables
+
+let selects = document.querySelectorAll(".select");
+
+selects.forEach(select => {
+    select.addEventListener("change" , (evt) => {
+        targetElemet(evt.target);
+    });
+});
